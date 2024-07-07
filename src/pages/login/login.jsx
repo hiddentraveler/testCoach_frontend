204c7e43746a25a321f6eb11106310d3f70ae8bf
hiddentraveler <github.cincholoipon@aleeas.com>
2024-07-07 19:24:07 +0530

    added login, signup calls to server

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,70 @@
+import { useState } from "react"
+
+async function getUser(email, pass, setMsg) {
+	const url = "http://localhost:8000/login";
+
+	const options = {
+		mode: "cors",
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Host": "http://localhost:8000/",
+			"Origin": "http://localhost:5173/"
+		},
+		body: JSON.stringify({
+			email: email,
+			pass: pass
+		})
+	};
+
+	try {
+
+		const response = await fetch(url, options);
+		const result = await response.json()
+		console.log(result);
+		if (result.error) {
+
+			setMsg(result);
+		} else {
+			setMsg({});
+			localStorage.setItem("user", JSON.stringify(result))
+		}
+	} catch (e) {
+		console.log(e);
+	}
+}
+
 export default function LogIn() {
+	const [email, setEmail] = useState("")
+	const [pass, setPass] = useState("")
+	const [msg, setMsg] = useState({})
+
+	function handleSubmit(e) {
+		e.preventDefault()
+		getUser(email, pass, setMsg)
+	}
+
+	if (localStorage.getItem('user')) {
+		location.replace('/');
+	}
 	return (
-		<div>
-			<h3>Log In</h3>
-		</div>
+		<>
+			{}
+			{!msg.msg ? "" : (
+				<div>
+					<span style={msg.error ? { color: "red" } : { color: "cyan" }}>{msg.msg}</span>
+				</div>
+			)
+			}
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="email">email:</label>
+				<input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} /> <br />
+
+				<label htmlFor="password">password:</label>
+				<input type="password" id="password" value={pass} onChange={(e) => setPass(e.target.value)} /> <br />
+
+				<button>Submit</button>
+			</form>
+		</>
 	)
 }
--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,7 +1,60 @@
+import { useState } from "react"
+
+async function adduser(email, pass, setErrMsg) {
+	const url = "http://localhost:8000/signup";
+
+	const options = {
+		mode: "cors",
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Host": "http://localhost:8000/",
+			"Origin": "http://localhost:5173/"
+		},
+		body: JSON.stringify({
+			email: email,
+			pass: pass
+		})
+	};
+
+	try {
+
+		const response = await fetch(url, options);
+		const result = await response.json()
+		console.log(result.msg);
+		setErrMsg(result);
+	} catch (e) {
+		console.log(e);
+	}
+}
+
 export default function SignUp() {
+	const [pass, setPass] = useState("")
+	const [email, setEmail] = useState("")
+	const [errMsg, setErrMsg] = useState({})
+
+	function handleSubmit(e) {
+		e.preventDefault()
+		adduser(email, pass, setErrMsg)
+
+	}
 	return (
-		<div>
-			<h3>Sign Up</h3>
-		</div>
+		<>
+			{!errMsg.msg ? "" : (
+				<div>
+					<span style={errMsg.error ? { color: "red" } : { color: "cyan" }}>{errMsg.msg}</span>
+				</div>
+			)
+			}
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="email">email:</label>
+				<input required type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} /> <br />
+
+				<label htmlFor="password">password:</label>
+				<input required type="password" id="password" value={pass} onChange={(e) => setPass(e.target.value)} /> <br />
+
+				<button>Submit</button>
+			</form>
+		</>
 	)
 }
